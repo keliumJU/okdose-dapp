51c1e2b3ae7fab0da051dbc804138f89e1c2ad26
norn <48634254+keliumJU@users.noreply.github.com>
2023-01-23 09:34:26 -0500

    Create smart contract for Miltefosine prescription (#39)
    * Add translations for miltefosine and automatically installed parcel plugins.
    
    * remove unneeded dependencies and files
    
    * Remove unnecessary App.js logic
    
    * Update submodule

--- okdose/transmissionTypes/transmittedByVectors/leishmaniasis/index.js
+++ okdose/transmissionTypes/transmittedByVectors/leishmaniasis/index.js
@@ -10,8 +10,13 @@ const prescribePentamidineIsethionate = async (nearInterface, weight) => (
   nearInterface.viewMethod(process.env.PENTAMIDINE_CONTRACT, process.env.CONTRACT_METHOD, { weight })
 );
 
+const prescribeMiltefosine = async (nearInterface, weight) => (
+  nearInterface.viewMethod(process.env.MILTEFOSINE_CONTRACT, process.env.CONTRACT_METHOD, { weight })
+);
+
 export {
   prescribeNMetilGlucamine,
   prescribeSodiumStibogluconate,
-  prescribePentamidineIsethionate
+  prescribePentamidineIsethionate,
+  prescribeMiltefosine
 };
--- okdoseweb/App.js
+++ okdoseweb/App.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import { NearInterface } from '@near-interface';
-import { prescribePentamidineIsethionate } from '@leishmaniasis';
+import { prescribeMiltefosine } from '@leishmaniasis';
 
 export default function App({ wallet }) {
   const { t } = useTranslation();
@@ -11,13 +11,13 @@ export default function App({ wallet }) {
   const weight = 50;
 
   React.useEffect(() => {
-    prescribePentamidineIsethionate(nearInterface, weight)
+    prescribeMiltefosine(nearInterface, weight)
       .then(setValueFromBlockchain)
       .catch(alert);
   }, []);
 
   const handleWeight = (event) => {
-    prescribePentamidineIsethionate(nearInterface, parseFloat(event.target.value))
+    prescribeMiltefosine(nearInterface, parseFloat(event.target.value))
       .then(setValueFromBlockchain)
       .catch(alert);
   };
@@ -29,14 +29,13 @@ export default function App({ wallet }) {
       <hr />
       <p>{ t('leishmaniasis.name') }</p>
       <p>{ t(valueFromBlockchain.name) }</p>
+      <p>{ valueFromBlockchain.weightDose }</p>
       <p>{ t(`${valueFromBlockchain.presentation}.0`) }</p>
+      <p>{ t(`${valueFromBlockchain.presentation}.1`) }</p>
       <p>{ t(`${valueFromBlockchain.treatmentTime}.0`) }</p>
-      <p>{ t(valueFromBlockchain.via) }</p>
       <p>{ t(valueFromBlockchain.frequency) }</p>
       <p>{ t(valueFromBlockchain.warning) }</p>
-      <p>{ t(`${valueFromBlockchain.preparation}.0`) }</p>
-      <p>{ valueFromBlockchain.volumeDose }</p>
-      <p>{ valueFromBlockchain.weightDose}</p>
+      <p>{ t(valueFromBlockchain.note) }</p>
     </main>
   );
 }
