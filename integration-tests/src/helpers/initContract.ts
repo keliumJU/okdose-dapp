887c81249737186b6f6fa9b3e249c8dd4862591c
Laura Villada <112585154+LauraVilladakommit@users.noreply.github.com>
2023-01-31 09:23:14 -0500

    Set up for integration tests for smart contracts (#51)
    * Set up and create integration tests
    
    * Add integration test route in package script
    
    * Modify the license in test-integration package
    
    * Access env variables from the project root
    
    * Refactor integration tests
    
    * Update the env file name in the start script
    
    * Add integration tests to the CI
    
    * Organize code style and remove test from the CI

---
+++ integration-tests/src/helpers/initContract.ts
@@ -0,0 +1,28 @@
+import { Worker } from 'near-workspaces';
+import { test } from './initTest'
+
+export default function initContract (contractAccount: string, blockId: string) {
+  // Init the worker and start a Sandbox server
+  test.beforeEach(async (t) => {
+    // Init the worker and start a Sandbox server
+    const worker = await Worker.init();
+
+    // Using contract from testnet
+    const root = worker.rootAccount;
+
+    const contract = await root.importContract({
+      testnetContract: contractAccount,
+      blockId: blockId,
+      withData: false,
+    });
+    // Save state for test runs, it is unique for each test
+    t.context.worker = worker;
+    t.context.accounts = {root, contract };
+  });
+
+  test.afterEach.always(async (t) => {
+    await t.context.worker.tearDown().catch(error => {
+      new Error(`Failed to tear down the worker: ${error}`)
+    });
+  });
+}
---
+++ integration-tests/src/pentamidineIsethionate.ava.ts
@@ -0,0 +1,25 @@
+import  initContract  from './helpers/initContract';
+import { test } from './helpers/initTest';
+import * as dotenv from 'dotenv';
+import { join } from 'path';
+dotenv.config({path: join(__dirname, '../../', '.env')})
+
+const contractAccount = process.env.PENTAMIDINE_CONTRACT as string;
+const blockId = process.env.PENTAMIDINE_CONTRACT_BLOCK_ID as string;
+const contractMethod = process.env.CONTRACT_METHOD as string;
+
+initContract(contractAccount, blockId)
+
+test('It returns the correct type attributes', async (t) => {
+  const { contract } = t.context.accounts;
+  const dose: any = await contract.view(contractMethod, { weight: 5 });
+
+  t.is(typeof dose.name, 'string');
+  t.is(typeof dose.presentation, 'string');
+  t.is(typeof dose.weightDose[0], 'string');
+  t.is(typeof dose.volumeDose[0],'string');
+  t.is(typeof dose.via, 'string');
+  t.is(typeof dose.frequency, 'string');
+  t.is(typeof dose.treatmentTime, 'string');
+  t.is(typeof dose.preparation, 'string');
+});
